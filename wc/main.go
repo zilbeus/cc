86c96ded1889e71283cf4318c50e6da76c45cf47
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-08-16 22:13:22 +0300

    add initial wc cmd with byte count option

--- wc/main.go
+++ wc/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var countBytes bool
+	flag.BoolVar(&countBytes, "c", false, "count number of bytes in file")
+	flag.Parse()
+
+	noFlagsSet := !countBytes
+	if noFlagsSet {
+		fmt.Printf("No count option given. Exiting.\n")
+		return
+	}
+
+	filename := flag.Arg(0)
+	if filename == "" {
+		fmt.Printf("Missing filename.\n")
+		return
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf(
+			"Unable to open file '%s' for reading. Does it exist?",
+			filename,
+		)
+		return
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Printf(
+			"Unable to query fileinfo for file '%s'.",
+			filename,
+		)
+		return
+	}
+
+	bytes := fileInfo.Size()
+
+	if countBytes {
+		fmt.Printf("%d %s\n", bytes, filename)
+		return
+	}
 }
